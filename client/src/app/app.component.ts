90239bdb4aa4cf94da5a16e6a1fb0f3705ae372c
Andrea Restelli <andrearestelli28@gmail.com>
2023-04-01 18:53:02 +0200

    added black background

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
 import * as AuthActions from './store/auth/auth.actions';
 import { Store } from '@ngrx/store';
 import * as fromApp from './store/app.reducers';
@@ -8,13 +8,18 @@ import * as fromApp from './store/app.reducers';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
-  constructor(private store: Store<fromApp.AppState>) {
+  constructor(private store: Store<fromApp.AppState>, private elementRef: ElementRef) {
   }
 
   ngOnInit(): void {
     this.store.dispatch(new AuthActions.CheckIfLoggedIn());
+  }
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument
+      .body.style.backgroundColor = 'black';
   }
 
   onActivate($event) {
